eeaf98f7b5e0a3b65a84c88eeaea5d7a926be0b3
Hamza Mounir <hamza.pixelle@gmail.com>
2018-06-14 20:20:38 +0200

    end exercice

--- src/App.js
+++ src/App.js
@@ -6,16 +6,45 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderForm = this.renderForm.bind(this);
     this.state = {
+      formData: {},
     }
   }
 
   handleSubmit(e){
     e.preventDefault();
     const inputs = this.refs;
+    const values = {};
     for(const field in inputs) {
-      console.log({ name: field, value: inputs[field].value});
+      values[field] = inputs[field].value;
     }
+    this.setState({ formData: values })
+  }
+  
+  renderForm() {
+    const whiteLabelModel= {
+      bigTitle: '',
+  		cardBackground: '',
+      error: '',
+      footerBackground: '',
+      footerText: '',
+      headerBackground: '',
+      primary: '',
+      primaryBackground: '',
+      primaryButtonText: '',
+      secondary: '',
+      secondaryButtonText: '',
+      smallFooterText: '',
+      textQuote: '',
+    }
+    return  Object.keys(whiteLabelModel)
+    .map((p) => {
+      return (<div key={p}>
+        <input type="text" placeholder={p} ref={p} name={p} /> 
+        <br /><br />
+      </div>);
+    });
   }
 
   render() {
@@ -26,13 +55,13 @@ class App extends React.Component {
           <h1 className="App-title">White Label Tool</h1>
         </header>
         <form onSubmit={this.handleSubmit}>
-          First name:
-          <input type="text" ref="firstname" name="firstname"/><br /><br />
-          Last name:
-          <input type="text" ref="lastname"  name="lastname"/><br />
+          {this.renderForm()}
           <button type="submit">Submit</button>
         </form>
         <br />
+        <p className="valueForm">
+          {JSON.stringify(this.state.formData, null, 2)}
+        </p>
       </div>
     );
   }

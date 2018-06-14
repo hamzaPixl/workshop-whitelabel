a21eb932223f73d4436d94d44d672a651023809f
Hamza Mounir <hamza.pixelle@gmail.com>
2018-06-14 19:37:01 +0200

    end of exercise

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.png';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+    }
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const inputs = this.refs;
+    for(const field in inputs) {
+      console.log({ name: field, value: inputs[field].value});
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +25,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">White Label Tool</h1>
         </header>
-        <p className="App-intro">
-          
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          First name:
+          <input type="text" ref="firstname" name="firstname"/><br /><br />
+          Last name:
+          <input type="text" ref="lastname"  name="lastname"/><br />
+          <button type="submit">Submit</button>
+        </form>
+        <br />
       </div>
     );
   }

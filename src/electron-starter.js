ec024031f91f81a696d3996e18083cf960870010
Hamza Mounir <hamza.pixelle@gmail.com>
2018-07-09 11:45:13 +0200

    add electron app

---
+++ src/electron-starter.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+require('electron-reload')(__dirname);
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    transparent: false,
+    frame: true,
+    width: 600,
+    height: 850,
+    webPreferences: { webSecurity: false },
+    show: true,
+    darkTheme: true,
+  });
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
+  mainWindow.loadURL(startUrl);
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('browser-window-created', (e, window) => {
+  window.setMenu(null);
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
